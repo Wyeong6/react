876a47e85a0de64b0f1ee9282b384c25699472d6
Wyeong6 <dnduddl1381@gmail.com>
2024-04-04 16:45:29 +0900

    123

--- task-app/src/App.jsx
+++ task-app/src/App.jsx
@@ -4,21 +4,34 @@ import TaskColumn from '../components/TaskColumn';
 import todoIcon from './assets/direct-hit.png';
 import doingIcon from './assets/glowing-star.png';
 import doneIcon from './assets/check-mark-button.png';
+import { useState,useEffect } from 'react';
+
+
+const saveTasks = localStorage.getItem('tasks');
+
 
 export default function App() {
 
+	
 
+	const handleDelete = (taskIndex) => {
+		const newTasks = tasks.filter((task, index) => index !== taskIndex);
+		setTasks(newTasks);
+	};
 
+	const[tasks,setTasks] = useState(JSON.parse(saveTasks) || []);
 
-
+	useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}, [tasks]);
 
 	return (
 		<div className='app'>
-			<TaskForm/>
+			<TaskForm setTasks={setTasks}/>
 			<main className='app_main'>
-        <TaskColumn title='할 일' icon={todoIcon} />
-				<TaskColumn title='진행중' icon={doingIcon} />
-				<TaskColumn title='완 료' icon={doneIcon} />
+        		<TaskColumn title='할 일' icon={todoIcon}  tasks={tasks} status='todo' handleDelete={handleDelete} />
+				<TaskColumn title='진행중' icon={doingIcon}  tasks={tasks} status='doing' handleDelete={handleDelete} />
+				<TaskColumn title='완 료' icon={doneIcon}  tasks={tasks} status='done' handleDelete={handleDelete} />
 			</main>
 		</div>
 	);
